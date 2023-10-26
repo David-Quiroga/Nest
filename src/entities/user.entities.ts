5eef769f460a64dd30932eca4ad03d91320a6ae0
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2023-10-25 20:49:13 -0500

    Se agregaron todas las entidades

--- src/entities/user.entities.ts
+++ src/entities/user.entities.ts
@@ -44,12 +44,12 @@ export class userEntity{
         comment: 'User Direction'
     })
     direction:string;
-    @Column('number', {
+    @Column('varchar', {
         name: 'phone',
         nullable: false,
         comment: 'Users Phone' 
     })
-    phone:number;
+    phone:string;
     @Column('varchar', {
         name: 'email',
         nullable: false,
@@ -66,17 +66,56 @@ export class userEntity{
 
     @BeforeInsert()
     @BeforeUpdate()
+    async setName() {
+        if(!this.name){
+            return
+        }
+        this.name = this.name.toUpperCase();
+    }
 
+    @BeforeInsert()
+    @BeforeUpdate()
+    async setLastName(){
+        if(!this.lastname){
+            return
+        }
+        this.lastname = this.lastname.toLowerCase();
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async setDirection() {
+        if (!this.direction) {
+            return;
+        }
+        this.direction = this.direction.toLocaleLowerCase("es");
+    }
+    
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async setPhone(){
+        if(!this.phone){
+            return
+        }
+        this.phone = this.phone.toString();
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async setEmail(){
+        if(!this.email){
+            return
+        }
+        this.email = this.email.trim();
+    }
+    @BeforeInsert()
+    @BeforeUpdate()
     async setRol(){
         if(!this.rol){
             return
         }
         this.rol =this.rol.substring(1, 2); //!en este caso, nos devuelve los caracteres que se encuentran en un rango de posiciones
     }
-
-    @BeforeInsert()
-    @BeforeUpdate()
-
-    async 
 }
 
